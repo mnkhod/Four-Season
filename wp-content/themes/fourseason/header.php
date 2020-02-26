096a7b71da5f8bd035008f4e8e521d2825b88289
Munkh-Od Ganzorigt <mnkhod@eikyuu.hoshi@gmail.com>
2020-02-26 17:45:37 +0800

    Adding Tailwind and Tailwind Config

--- wp-content/themes/fourseason/header.php
+++ wp-content/themes/fourseason/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
 
+    <!-- Tailwind Base CSS -->
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/prod/css/base.css">
     <?php wp_head(); ?>
 </head>
 <body>
